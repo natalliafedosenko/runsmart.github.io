b57c4e4121dd6d6ddce4627f34e78653250228fc
Natallia <example@gmail.com>
2024-02-05 19:43:13 +0300

    final

--- src/js/script.js
+++ src/js/script.js
@@ -82,6 +82,35 @@ $(document).ready(function(){
 
   $('input[name=phone]').mask("+375 (99) 999-99-99");
 
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+      $(window).scroll(function(){
+        if($(this).scrollTop()>1600){
+          $('.pageup').fadeIn();
+        } else{$('.pageup').fadeOut()}
+      });
+
+      $("a[href=#up]").click(function(){
+        const _href=$(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top="px"});
+            return false;
+      });
+      new WOW().init();
 });
 
 
